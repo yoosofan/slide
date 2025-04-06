71547acac1c59d1fb78e1f377dfa9a1401156be5
Ahmad Yoosofan <yoosofan@gmx.com>
2025-04-06 12:16:10 +0330

    Change and Automatic update: Sun 06 Apr 2025 12:16:10 PM +0330

--- os/src/thread/241.py
+++ os/src/thread/241.py
@@ -1,16 +1,25 @@
-import threading # src/thread/240.py
+import threading # src/thread/241.py
 def f1():
+  k = 0
   for i in range(1000000):
+    for j in range(100000):
+      k+=1
     print(i,"__111")
 
 def f2():
+  k = 0
   for i in range(1000000):
+    for j in range(100000):
+      k+=1
     print(i,"__222")
 
 if __name__ == "__main__":
+  k = 0
   th1 = threading.Thread(target=f1)
   th2 = threading.Thread(target=f2)
   th1.start();
   th2.start();
   for i in range(1000000):
+    for j in range(100000):
+      k+=1
     print(i, "__main")
