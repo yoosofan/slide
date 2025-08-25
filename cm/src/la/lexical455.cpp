d7fe6e39acf5283238094a13eab05e2d45647901
Ahmad Yoosofan <yoosofan@gmx.com>
2025-08-25 13:11:39 +0330

    Change and Automatic update: Mon 25 Aug 2025 01:11:39 PM +0330

--- cm/src/la/lexical455.cpp
+++ cm/src/la/lexical455.cpp
@@ -1,8 +1,14 @@
 #include<iostream>
 using namespace std;
+
+string typeString[] = {
+  "INT", "PLUS", "MINUS", "MUL",
+  "DIV", "OP", "CP", "EOI"
+};
+ 
 enum TokenType{
-  NUMBER, PLUS, MINUS, MUL,
-  DIV, LPAR, RPAR, EOF_INPUT
+  INT, PLUS, MINUS, MUL,
+  DIV, OP, CP, EOI
 };
 struct Token{
   TokenType type;
@@ -16,48 +22,24 @@ class lexical{
   Token  t;
   char buffer[200];
   int tindex = 0;
+
   public:
+
   void read(void){
-    cout << "Enter expression"
-      << endl;
-    cin>>buffer;
+    cout << "Enter: ";
+    cin.get(buffer,200);
   }
-  Token getToken(void){
-    static int number_of_call = 0;
-    number_of_call ++;
-    if(buffer[tindex] == '(')        
-      t.type = LPAR;
-    else if(buffer[tindex] == ')')   
-      t.type = RPAR;
-    else if(buffer[tindex] == '+')   
-      t.type = PLUS;
-    else if(buffer[tindex] == '-')   
-      t.type = MINUS;
-    else if(buffer[tindex] == '*')   
-      t.type = MUL;
-    else if(buffer[tindex] == '/')   
-      t.type = DIV;
-    else if(buffer[tindex]>='0' && 
-      buffer[tindex]<='9')
-    {
-      t.type=NUMBER; t.n = 0;
-      for(; buffer[tindex] >= '0' && 
-        buffer[tindex] <= '9'; tindex++)
-          t.n=t.n*10+buffer[tindex]-48;
-      tindex --;
-    }else if(buffer[tindex] == '\0')  
-      t.type = EOF_INPUT;
-    else    
-      error("unknown character");
-    tindex++; 
-    return t;
-  }
+
   void printToken(Token t){
     cout<< "tindex: "<< tindex
-      << "\t type: "<<t.type 
+      << "\t type: "
+      <<typeString[int(t.type)]
       <<"\t int "<< t.n << endl;
   }
+
+  Token getToken(void);
 };
+
 int main(){
   lexical lex1;
   Token t;
@@ -65,5 +47,26 @@ int main(){
   do{
     t=lex1.getToken();
     lex1.printToken(t);
-  }while(t.type != EOF_INPUT);
+  }while(t.type != EOI);
+}
+
+Token lexical::getToken(void){
+  t.n = -1;
+  if(buffer[tindex] == '(')        t.type = OP;
+  else if(buffer[tindex] == ')')   t.type = CP;
+  else if(buffer[tindex] == '+')   t.type = PLUS;
+  else if(buffer[tindex] == '-')   t.type = MINUS;
+  else if(buffer[tindex] == '*')   t.type = MUL;
+  else if(buffer[tindex] == '/')   t.type = DIV;
+  else if(buffer[tindex] == '\0')  t.type = EOI;
+
+  else if(buffer[tindex]>='0' && buffer[tindex]<='9'){
+    t.type=INT; t.n = 0;
+    for(; buffer[tindex] >= '0' && buffer[tindex] <= '9'; tindex++)
+        t.n=t.n*10+buffer[tindex]-48;
+    tindex --;
+  }
+  else    error("unknown character");
+  tindex++; 
+  return t;
 }
