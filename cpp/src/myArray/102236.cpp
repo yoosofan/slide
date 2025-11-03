c60b591ac307c28f04c2eb34fa9c17cfaacc34f6
Ahmad Yoosofan <yoosofan@gmx.com>
2025-11-03 17:03:36 +0330

    Change and Automatic update: Mon Nov  3 05:03:36 PM +0330 2025

---
+++ cpp/src/myArray/102236.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+class myArray{int n=0;//102236.cpp
+  double *a=nullptr; public:
+  myArray(const double *ma=nullptr,
+      int n = 0){// Error
+    if(n == 0 || ma == nullptr)
+    {n = 0; a = nullptr;}
+    else{a=new double[this->n=n];
+      for(int i=0;i<n;i++)a[i]=ma[i];
+    }
+  }
+  ~myArray(){delete[] a; a=nullptr;
+    n=0; cout<<"Destructor"<<endl;
+  }
+  double get(const int index = 0 ){
+    if(index < n) return a[index];
+    cout<<"Error:get"<<endl;
+    return 0;
+  }
+  void set(const int index,
+           const double value){
+    if(index<n) a[index] = value;
+    else cout<<"Error:set"<<endl;
+  }
+  void print(){
+    cout << " n = " << n << endl;
+    for(int i = 0; i < n; i++)
+      cout << a[i] << endl;
+  }
+};
+void f2(myArray);
+int main(){double x[]={10,12,34,54};
+  myArray d(x, 4);  myArray p(d);
+  cout<<"before f2"<<endl;  f2(p);
+}
+void f2(myArray k){k.print();}/*
+copy constructor
+before call f2
+copy constructor
+ n = 4
+a[0] = 10
+a[1] = 12
+a[2] = 34
+a[3] = 54
+in destructor
+in destructor
+in destructor*/
---
+++ cpp/src/shape_side/side2.cpp
@@ -0,0 +1,68 @@
+/*
+ * Copyright 2018 Ahmad Yoosofan <yoosofan@myfastmail.com>,
+ * http://yoosofan.github.io;
+ * http://yoosofan.kashanu.ac.ir
+ * University of Kashan (http://kashanu.ac.ir)
+ * 2014/05/03
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+*/
+#include <iostream>
+#include <cmath>
+using namespace std;
+struct side{// Error
+  int d;
+  side(int a){
+    if(a<=0){cout<<"Side of a shape connot be negative or zero"<<endl;exit(0);}
+    d=a;
+  }
+  side(const side&a){d = a.d;cout<<"copy constructor:"<<d<<endl;}
+  void input(void){
+    cout<<"Enter length of side:"; cin>>d;
+    while(d<=0){
+      cout<<"Side must be >0"<<endl;
+      cout<<"Enter length of side:"; cin>>d;
+    }
+  }
+  void print(){cout << d;}
+};
+class triangle{
+  side a, b, c;
+  bool check(void){return (a.d+b.d>c.d && a.d+c.d>b.d && b.d+c.d>a.d);}// true, false
+  public:
+  triangle(const side& a, const side& b, const side& c){
+      this->a=a;
+      this->b=b;
+      this->c=c;
+      if(!check()){
+	    cout<<"These sides do not generate a triangle: ";
+	    print();
+    }
+  }
+  triangle(const triangle& d)
+  {a=d.a; b=d.b; c=d.c; cout << "copy constructor" << endl;}
+  void print(){cout<<"\t";a.print();cout<<",\t";b.print();cout<<",\t";c.print();cout<<endl;}
+  void input(){
+    //int k;
+    do{
+      //cout<<"Enter a: ";cin>>k;
+      //a=k;
+      a.input();
+      //cout<<"Enter b: ";cin>>k;
+      //b=k;
+      b.input();
+      //cout<<"Enter c: ";cin>>k;
+      //c=k;
+      c.input();
+    }while(!check());
+  }
+};
+void f2(){
+	side a(3);
+	side b = a;
+	a.print();
+	b.print();
+}
+int main(){f2();}
