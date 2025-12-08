9047a81119185f90bcc489b0f366c942a71bc8e2
Ahmad Yoosofan <yoosofan@gmx.com>
2025-12-08 15:13:02 +0330

    Change and Automatic update: Mon Dec  8 03:13:02 PM +0330 2025

---
+++ cpp/src/polym06.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+class Shape{protected://abstract
+  string name, color; public:
+  Shape(string name="shape",
+    string color="white"):
+      name(name),color(color)
+  {}
+  virtual int Area() = 0;//abstract
+};
+class Square: public Shape{
+  int d; public:
+  virtual int Area(){
+    return d*d;
+  }
+  Square(int d=1,
+    string name="square",
+    string color="blue"):
+      Shape(name, color)
+  {this->d=d;}
+  void printAll(void){
+    cout << "name: " <<
+      this->name << '\t'
+      << "color: " <<
+      this->color << '\t' <<
+      "d: " << this->d << endl;
+  }
+};
+void f2(Square p)
+{cout<<p.Area()<<endl;}
+void f1(Shape* p)
+{cout<<p->Area()<<endl;}
+int main(){
+  Square sq(3);
+  f2(sq); f1(&sq);
+}
