338a65bf32f085f6c841acc9bddd96bb40d4e354
Ahmad Yoosofan <yoosofan@gmx.com>
2025-12-09 12:57:35 +0330

    Change and Automatic update: Tue Dec  9 12:57:35 PM +0330 2025

--- cpp/src/in/shape10.cpp
+++ cpp/src/in/shape10.cpp
@@ -62,8 +62,8 @@ void f1(Shape* p){
   cout<<p->Area()<<endl;
   p->print();
 }
-int main(){
+int main(){Rectangle r1(4,5);
   Square sq(3);
   Circle c1(5);
-  f1(&sq); f1(&c1);
+  f1(&sq); f1(&c1); f1(&r1);
 }
